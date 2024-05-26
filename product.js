2b7f1f0b8d56b2f919bfdc642320d443e6a5a32d
Citralyn <alexaduffy1414@gmail.com>
2024-05-25 21:19:59 -0700

    project.js connects with mongodb SheCommerce database, can query product csv file in database to return relevant company

---
+++ product.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+const uri = process.env.MONGO_KEY;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('SheCommerceDB');
+    const collection = db.collection('products');
+
+    // Find the first document in the collection
+    const first = await collection.findOne({Product_Types : {$regex : "pillow"}});
+
+    console.log(first);
+  } finally {
+    // Close the database connection when finished or an error occurs
+    await client.close();
+  }
+}
+run().catch(console.error);
